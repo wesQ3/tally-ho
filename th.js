0f34ad7e3c7f46b979af9f173a8913a7e34a9180
Wes Malone <wesdmalone@gmail.com>
2011-11-13 22:26:24 -0600

    switched to underscore.js - cool!

--- th.js
+++ th.js
@@ -1,32 +1,19 @@
-String.prototype.format = function () {
-  var args = arguments;
-  return this.replace(/\{(\d+)\}/g, function (m, n) { return args[n]; });
-};
-
 var keys = {};
+var makeRow = 
+   _.template('<tr id="<%= char %>"><td><%= char %></td><td class="counter"><%= val %></td></tr>');
 var keyHandler = function(event) {
    var char = String.fromCharCode(event.which);
    if (!char.match(/[A-Za-z0-9]/)) return;
    if (!keys[char]) keys[char] = 0;
    keys[char]++;
-   //console.log(char, keys[char]);
-   var $tr = $('#'+char);
-   if ($tr.length === 0) {
-      //console.log('new row');
-      var row = '<tr id="{0}"><td>{0}</td><td class="counter">{1}</td></tr>';
-      $('#results > tbody:last').append(row.format(char,keys[char]));
-   }
-   $tr.children('.counter').text(keys[char]);
-   sort();
-};
 
-var sort = function() {
-   var l = $('#results').find('tr').detach();
-   l = l.sort(function(a,b) {
-      if ( keys[a.id] == keys[b.id] ) return 0;
-      return keys[a.id] < keys[b.id] ? 1 : -1;
-   });
-   $('#results').append(l);
+   $('#results > tbody').empty();
+   var rows = _(keys).chain().keys()
+      .sortBy(function(char) { return -keys[char] })
+      .map(function(char) { return makeRow({ char: char, val: keys[char] })})
+      .value().join('');
+
+   $('#results > tbody').append(rows);
 };
 
 $(document).ready(function() {
