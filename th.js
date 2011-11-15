26cc6ee51d44369871b6eba58b28b1e2cabe47d0
Wes Malone <wes@mitsi.com>
2011-11-14 19:41:08 -0600

    added percentages, total, and reset button

--- th.js
+++ th.js
@@ -1,6 +1,6 @@
-var keys = {};
+var keys = {}, per;
 var makeRow = 
-   _.template('<tr id="<%= char %>"><td><%= char %></td><td class="counter"><%= val %></td></tr>');
+   _.template('<tr id="<%= char %>"><td><%= char %></td><td class="counter"><%= val %></td><td><%= per %></td></tr>');
 var keyHandler = function(event) {
    var char = String.fromCharCode(event.which);
    if (!char.match(/[A-Za-z0-9]/)) return;
@@ -8,14 +8,23 @@ var keyHandler = function(event) {
    keys[char]++;
 
    $('#results > tbody').empty();
+   var total = _(keys).chain().values().reduce(function(a,b) { return a + b }).value();
    var rows = _(keys).chain().keys()
       .sortBy(function(char) { return -keys[char] })
-      .map(function(char) { return makeRow({ char: char, val: keys[char] })})
+      .map(function(char) { return makeRow(
+         { char: char, val: keys[char], per: ( 100*keys[char]/total ).toFixed(2) })})
       .value().join('');
 
-   $('#results > tbody').append(rows);
+   var total_row = makeRow({ char: 'Total', val: total });
+
+   $('#results > tbody').append(rows + total_row);
 };
 
 $(document).ready(function() {
    $(document).keypress(keyHandler);
+   $('#reset').click(function() {
+      keys = {}; 
+      $('#results > tbody').empty()
+         .append('<tr><td>Start typing keys to begin tallying...</td></tr>');
+   });
 });
