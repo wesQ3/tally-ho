78ca63308e5c6eac91094f94c9074f4171f5c506
Wes Malone <wesdmalone@gmail.com>
2011-12-15 21:53:51 -0600

    use objects in key hash to support aliasing

--- th.js
+++ th.js
@@ -1,30 +1,36 @@
 var keys = {}, per;
 var makeRow = 
    _.template('<tr id="<%= char %>"><td><%= char %></td><td class="counter"><%= val %></td><td><%= per %></td></tr>');
+
 var keyHandler = function(event) {
    var char = String.fromCharCode(event.which);
    if (!char.match(/[A-Za-z0-9]/)) return;
-   if (!keys[char]) keys[char] = 0;
-   keys[char]++;
+   if (!keys[char]) { keys[char] = { count: 0, alias: char } };
+   keys[char].count++;
 
-   $('#results > tbody').empty();
-   var total = _(keys).chain().values().reduce(function(a,b) { return a + b }).value();
+   var total = _(keys).chain().values()
+      .map(function(o) { return o.count })
+      .reduce(function(a,b) { return a + b }).value();
    var rows = _(keys).chain().keys()
-      .sortBy(function(char) { return -keys[char] })
+      .sortBy(function(char) { return -keys[char].count })
       .map(function(char) { return makeRow(
-         { char: char, val: keys[char], per: ( 100*keys[char]/total ).toFixed(2) })})
+         { char: keys[char].alias, val: keys[char].count, 
+            per: ( 100*( keys[char].count )/total ).toFixed(2) 
+         })})
       .value().join('');
 
    var total_row = makeRow({ char: 'Total', val: total });
 
-   $('#results > tbody').append(rows + total_row);
+   $('#results > tbody').empty().append(rows + total_row);
+};
+
+var resetKeys = function() {
+   keys = {}; 
+   $('#results > tbody').empty()
+      .append('<tr><td>Start typing keys to begin tallying...</td></tr>');
 };
 
 $(document).ready(function() {
    $(document).keypress(keyHandler);
-   $('#reset').click(function() {
-      keys = {}; 
-      $('#results > tbody').empty()
-         .append('<tr><td>Start typing keys to begin tallying...</td></tr>');
-   });
+   $('#reset').click(resetKeys);
 });
